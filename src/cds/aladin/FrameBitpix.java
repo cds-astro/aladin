2689df370a3aa187a68d1ffa0c1df111997d913b
fernique <Pierre.Fernique@astro.u-strasbg.fr>
2011-09-12 07:31:43 +0000

    Conversion de BITPIX

---
+++ src/cds/aladin/FrameBitpix.java
@@ -0,0 +1,206 @@
+// Copyright 2010 - UDS/CNRS
+// The Aladin program is distributed under the terms
+// of the GNU General Public License version 3.
+//
+//This file is part of Aladin.
+//
+//    Aladin is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, version 3 of the License.
+//
+//    Aladin is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    The GNU General Public License is available in COPYING file
+//    along with Aladin.
+//
+
+
+package cds.aladin;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import cds.tools.Util;
+
+/**
+ * Gestion de la fenetre associee à la conversion BITPIX d'une image
+ *
+ * @author Pierre Fernique [CDS]
+ * @version 1.0 : (sept 2011) Creation
+ */
+public final class FrameBitpix extends FrameRGBBlink {
+
+   private String TITLE,INFO,HELP1,CUT,CUTLABEL,CAST,PLANE;
+
+   private static int [] BITPIX = { -64,-32,64,32,16,8};
+   private static String [] CODE = { "double (-64)","float (-32)","long (64)",
+                                     "integer (32)","short (16)","byte (8)" };
+   private JRadioButton [] rb;
+   private JRadioButton cut;
+   private JLabel srcSize,trgSize;
+   private ButtonGroup cbBitpix; 
+
+   @Override
+   protected void createChaine() {
+      super.createChaine();
+      TITLE = a.chaine.getString("BITPIXTITLE");
+      INFO  = a.chaine.getString("BITPIXINFO");
+      HELP1  = a.chaine.getString("BITPIXHELP");
+      CUTLABEL    = a.chaine.getString("BITPIXCUTLABEL");
+      CUT    = a.chaine.getString("BITPIXCUT");
+      CAST    = a.chaine.getString("BITPIXCAST");
+   }
+
+   /** Creation du Frame gerant la creation d'un plan RGB. */
+   protected FrameBitpix(Aladin aladin) {
+      super(aladin);
+      Aladin.setIcon(this);
+   }
+
+   @Override
+   protected String getTitre() { return TITLE; }
+
+   @Override
+   protected String getInformation() { return INFO; }
+
+   @Override
+   protected String getHelp() { return HELP1; }
+
+   @Override
+   protected int getToolNumber() { return -2; }
+   @Override
+   protected int getNb() { return 1; }
+
+   @Override
+   protected String getLabelSelector(int i) {
+      return PLANE;
+   }
+
+   /** Recupere la liste des plans images valides */
+   @Override
+   protected PlanImage[] getPlan() {
+      Vector<Plan> v  =a.calque.getPlans(Plan.IMAGE);
+      if( v==null ) return new PlanImage[0];
+      PlanImage pi [] = new PlanImage[v.size()];
+      v.copyInto(pi);
+      return pi;
+   }
+
+   @Override
+   protected Color getColorLabel(int i) {
+      return Color.black;
+   }
+   
+   @Override
+   protected JPanel getAddPanel() {
+      GridBagConstraints c=new GridBagConstraints();
+      GridBagLayout g=new GridBagLayout();
+      c.fill=GridBagConstraints.BOTH;
+      c.gridwidth=GridBagConstraints.REMAINDER;
+
+      JPanel p=new JPanel();
+      p.setLayout(g);
+      
+      cbBitpix=new ButtonGroup();
+      JPanel pp1=new JPanel();
+      JLabel l = new JLabel("Bitpix:"); l.setFont(l.getFont().deriveFont(Font.BOLD));
+      pp1.add(l);
+      JRadioButton cb;
+      rb = new JRadioButton[ BITPIX.length ];
+      JPanel pbitpix = new JPanel( new GridLayout(2,3));
+      for( int i=0; i<rb.length; i++ ) {
+         rb[i]=cb=new JRadioButton(CODE[i]); cb.setActionCommand(CODE[i]);
+         pbitpix.add(cb); cbBitpix.add(cb);
+         cb.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) { updateTrg(); }
+         });
+      }
+      pp1.add(pbitpix);
+      g.setConstraints(pp1,c);
+      p.add(pp1);
+      
+      ButtonGroup cbg=new ButtonGroup();
+      JPanel pp=new JPanel();
+      l = new JLabel(CUTLABEL); l.setFont(l.getFont().deriveFont(Font.BOLD));
+      pp.add(l);
+      cut=cb=new JRadioButton(CUT); cb.setActionCommand(CUT);
+      cbg.add(cb); pp.add(cb);  cb.setSelected(true);
+      cb=new JRadioButton(CAST); cb.setActionCommand(CAST);
+      cbg.add(cb); pp.add(cb);
+      g.setConstraints(pp,c);
+      p.add(pp);
+      
+      pp=new JPanel();
+      srcSize = l= new JLabel("."); l.setFont(l.getFont().deriveFont(Font.ITALIC));
+      pp.add(l);
+      g.setConstraints(pp,c);
+      p.add(pp);
+      pp=new JPanel();
+      trgSize = l= new JLabel("."); l.setFont(l.getFont().deriveFont(Font.ITALIC));
+      pp.add(l);
+      g.setConstraints(pp,c);
+      p.add(pp);
+      
+
+      return p;
+   }
+
+   @Override
+   protected void submit() {
+      try {
+         PlanImage p=getPlan(ch[0]);
+         String code = cbBitpix.getSelection().getActionCommand();
+         int i = Util.indexInArrayOf(code, CODE);
+         int fct = cut.isSelected() ? PlanImageAlgo.BITPIXCUT : PlanImageAlgo.BITPIX;
+         a.calque.newPlanImageAlgo(null,p,null,fct,0,""+BITPIX[i],0);
+
+      } catch ( Exception e ) {
+         if( a.levelTrace>=3 ) e.printStackTrace();
+         Aladin.warning("Bitpix convertion failed !");
+      }
+   }
+   
+   private void updateTrg() {
+      PlanImage p = getPlan(ch[0]);
+      if( p==null ) return;
+      String code = cbBitpix.getSelection().getActionCommand();
+      int i = Util.indexInArrayOf(code, CODE);
+      int bitpix=BITPIX[i];
+      int width=p.naxis1;
+      int height=p.naxis2;
+      int n = Math.abs(bitpix)/8;
+      long size = (long)width*(long)height*(long)n;
+      trgSize.setText("Target size: "+width+"x"+height+" x "+n+" => "+Util.getUnitDisk(size));
+   }
+
+   
+   private void updateSrc(int bitpix,int width,int height) {
+      for( int i=0; i<BITPIX.length; i++ ){
+         rb[i].setSelected( BITPIX[i]==bitpix );
+         rb[i].setEnabled( BITPIX[i]!=bitpix );
+      }
+      int n = Math.abs(bitpix)/8;
+      long size = (long)width*(long)height*(long)n;
+      srcSize.setText("Current size: "+width+"x"+height+" x "+n+" => "+Util.getUnitDisk(size));
+   }
+
+   @Override
+   protected void adjustWidgets() {
+      srcSize.setText("");
+      trgSize.setText("");
+      PlanImage p = getPlan(ch[0]);
+      if( p==null ) return;
+      updateSrc(p.bitpix,p.naxis1,p.naxis2);
+   }
+}
---
+++ src/cds/fits/CacheFits.java
@@ -0,0 +1,226 @@
+// Copyright 2010 - UDS/CNRS
+// The Aladin program is distributed under the terms
+// of the GNU General Public License version 3.
+//
+//This file is part of Aladin.
+//
+//    Aladin is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, version 3 of the License.
+//
+//    Aladin is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    The GNU General Public License is available in COPYING file
+//    along with Aladin.
+//
+
+package cds.fits;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import cds.tools.Util;
+
+
+/**
+ * Classe de manipulation d'un cache de fichiers FITS ouverts.
+ * Permet d'éviter les ouvertures et réouvertures inutiles lors d'un traitement nécessitant plusieurs fois
+ * l'accès aux mêmes images.
+ * Il faut mentionner à la création la taille max allouée au cache, en Mo (par défaut 512Mo) et en Nb de fichiers (par défaut 10000)
+ * L'accès à un fichier Fits se fait par Fits getFits(filename);
+ * 
+ * @author Pierre Fernique [CDS]
+ * @version 1.0 - sept 2011
+ */
+public class CacheFits {
+
+   static private final long DEFAULT_MAXMEM=512L;
+   static private final int  DEFAULT_MAXFILE=10000;
+   
+   private long maxMem;     // Taille max (en octets)
+   private int  maxFile;    // Nombre de fichiers max
+   private long mem;        // mémoire courante (en octets)
+   private int file;        // Nombre de fichiers gérés
+   private int nextId;      // prochain identificateur unique de fichier
+   private HashMap<String, FitsFile> map;      // Table des fichiers
+   TreeMap<String,FitsFile> sortedMap;        // Table trié par ordre de dernier accès
+   
+   private int statNbOpen,statNbFind;
+   
+   /**
+    * Création d'un cache de fichiers Fits
+    */
+   public CacheFits() { this(DEFAULT_MAXMEM,DEFAULT_MAXFILE); }
+   
+   /**
+    * Création d'un cache de fichiers Fits
+    * @param maxMem limite en Mo occupés
+    * @param maxFile limite en nombre de fichiers ouverts simultanément
+    */
+   public CacheFits(long maxMem,int maxFile) {
+      this.maxMem=maxMem*1024L*1024L;
+      this.maxFile=maxFile;
+      mem=0L;
+      file=0;
+      nextId=0;
+      statNbOpen=statNbFind=0;
+      map = new HashMap<String, FitsFile>(maxFile);
+      sortedMap = new TreeMap<String, FitsFile>( new ValueComparator(map) );
+   }
+   
+   /**
+    * Récupération d'un Fits spécifié par son nom de fichier. 
+    * @param fileName Nom du fichier Fits à accéder (supporte le mode mosaic nomfichier[x,y-wxh])
+    * @return l'objet Fits
+    * @throws Exception
+    */
+   synchronized public Fits getFits(String fileName) throws Exception {
+      FitsFile f = find(fileName);
+      
+      // Trouvé, je le mets à jour
+      if( f!=null ) {
+         f.update();
+         statNbFind++;
+      }
+      
+      // Pas trouvé, je l'ajoute
+      else {
+         if( isOver() ) clean();
+         f=add(fileName);
+         statNbOpen++;
+      }
+      
+      return f.fits;
+      
+   }
+   
+   // Retrouve l'objet Fits dans le cache, null si inconnu
+   private FitsFile find(String name) { return map.get(name); }
+   
+   // Ajoute un fichier Fits au cache. Celui-ci est totalement chargé en mémoire
+   private FitsFile add(String name) throws Exception {
+      FitsFile f = new FitsFile();
+      f.fits = new Fits();
+      f.fits.loadFITS(name);
+      map.put(name, f);
+      mem+=f.getMem(); ;
+      file++;
+      return f;
+   }
+   
+   // Supprime un objet Fits du cache
+   private void remove(String name) {
+      FitsFile f=find(name);
+      if( f==null ) return;
+      mem-=f.getMem();
+      file--;
+      map.remove(name);
+   }
+   
+   // Retourne true si le cache est en surcapacité
+   private boolean isOver() {
+      return mem>maxMem || file>maxFile;
+   }
+   
+   // Retourne true si le cache est rempli à moins des 3/4
+   private boolean isMemOk() {
+      return mem<3*(maxMem/4) && file<3*(maxFile/4);
+   }
+   
+   // Supprime les plus vieux éléments du cache pour qu'il ne soit rempli qu'au 3/4
+   private void clean() {
+      sortedMap.clear();
+      sortedMap.putAll(map);
+      for( String key : sortedMap.keySet() ) {
+         if( isMemOk() ) return;
+         remove(key);
+//         System.out.println("clean.remove "+key );
+      }
+   }
+   
+   public String toString() { return "CacheFits: "+file+" file(s) for "+Util.getUnitDisk(mem)+" opened="+statNbOpen+" found="+statNbFind; }
+   
+   // Gère une entrée dans le cache
+   class FitsFile {
+      Fits fits;
+      long timeAccess;
+      
+      private int id;
+      
+      FitsFile() {
+         timeAccess = System.currentTimeMillis();
+         id=nextId++;
+      }
+      
+      public long getMem() {
+         if( fits==null ) return 0L;
+         return fits.widthCell*fits.heightCell*Math.abs(fits.bitpix)/8; 
+      }
+      
+      void update() { timeAccess = System.currentTimeMillis(); }
+      
+      public String toString() {
+         long now = System.currentTimeMillis();
+         return "["+id+"] age="+(now-timeAccess)+" => "+fits.getFilename();
+      }
+   }
+   
+   class ValueComparator implements Comparator {
+      Map base;
+      public ValueComparator(Map base) {
+          this.base = base;
+      }
+
+      // dans le cas d'égalité sur l'age du fichier, distingue le résultat en fonction
+      // du numéro unique (id). Sinon on perd un des éléments lors du balayage
+      public int compare(Object a, Object b) {
+         FitsFile a1 = (FitsFile)base.get(a);
+         FitsFile b1 = (FitsFile)base.get(b);
+         if( a1==null ) return 1;
+         if( b1==null ) return -1;
+         int val = (int)( b1.timeAccess+b1.id - a1.timeAccess );
+         if( val==0 ) return b1.id-a1.id;
+         return val;
+      }
+    }
+   
+//   static final String NAME1 = "C:/2MASS.fits";
+//   static final String NAME2 = "C:/Documents and Settings/Standard/Bureau/CFHTLS_W_g_222054+011900_T0007.fits[100,100-1000x1000]";
+//   static final String NAME3 = "C:/Test.fits";
+//   static final String NAME4 = "C:/Documents and Settings/Standard/Bureau/CFHTLS_W_g_222054+011900_T0007.fits[200,100-500x500]";
+//   
+//   static public void main(String argv[]) {
+//      try {
+//         CacheFits cf = new CacheFits(70*1024*1024,2);
+//         Fits f1 = cf.getFits(NAME1);
+//         System.out.println("f1 = "+f1);
+//         System.out.println(cf);
+//         Fits f2 = cf.getFits(NAME2);
+//         System.out.println("f2 = "+f2);
+//         System.out.println(cf);
+//         Fits f3 = cf.getFits(NAME3);
+//         System.out.println("f3 = "+f3);
+//         System.out.println(cf);
+//         Fits f4 = cf.getFits(NAME4);
+//         System.out.println("f4 = "+f4);
+//         System.out.println(cf);
+//         f3 = cf.getFits(NAME3);
+//         System.out.println("f3 = "+f3);
+//         System.out.println(cf);
+//         f4 = cf.getFits(NAME4);
+//         System.out.println("f4 = "+f4);
+//         System.out.println(cf);
+//         f2 = cf.getFits(NAME2);
+//         System.out.println("f2 = "+f2);
+//         System.out.println(cf);
+//
+//
+//      } catch( Exception e ) { e.printStackTrace(); }
+//   }
+
+}
