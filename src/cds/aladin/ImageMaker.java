b0cff44c01c9646dca96728160d64369ee344b02
thomasboch <thomas.boch@astro.unistra.fr>
2015-11-25 09:11:41 +0000

    Changed visibility of constructor and createAladin method to allow for use outside package

--- src/cds/aladin/ImageMaker.java
+++ src/cds/aladin/ImageMaker.java
@@ -31,50 +31,61 @@ import javax.imageio.ImageIO;
 
 /**
 * Generateur d'image statique
-* 
+*
 * @author Pierre Fernique [CDS]
 * @version 1.0 : oct 2015
 */
 public class ImageMaker {
 
-   protected ImageMaker(Aladin aladin,OutputStream output, String label, int width, int height, String fmt, Coord c, double radius) throws Exception {
-      
+
+   public ImageMaker(Aladin aladin,OutputStream output, String label, int width, int height,
+                     String fmt, Coord c, double radius) throws Exception {
+       this(aladin, output, label, width, height, fmt, c, radius, null);
+   }
+
+
+   public ImageMaker(Aladin aladin,OutputStream output, String label, int width, int height,
+                     String fmt, Coord c, double radius, String url) throws Exception {
+
+      if (url==null) {
+          url = "http://alasky.u-strasbg.fr/DSS/DSSColor";
+          /* url = "C:\\Users\\Pierre\\.aladin\\Cache\\Background\\CDS_P_DSS2_color" */
+
+      }
       // Generation d'un PlanBG de travail
-      PlanBG p = new PlanBGStatic(aladin, label,
-            /* "C:\\Users\\Pierre\\.aladin\\Cache\\Background\\CDS_P_DSS2_color" */
-             "http://alasky.u-strasbg.fr/DSS/DSSColor");
-      
+      PlanBG p = new PlanBGStatic(aladin, label, url);
+
 //      ViewSimpleStatic vs = (ViewSimpleStatic) aladin.view.getCurrentView();
       ViewSimpleStatic vs = new ViewSimpleStatic(aladin);
       ((ViewStatic)aladin.view).setViewSimple(vs);
       vs.setViewParam(p, width,height, c,radius);
-      
+
       // Il me faut un BufferImage de travail
       BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
       Graphics2D g = (Graphics2D) img.getGraphics();
-      
+
       // L'arrière fond
       p.drawBackground(g, vs);
-      
+
       // Génération d'une vue de travail
       // Dessin de l'image de fond
       p.drawLosangesNow(g, vs);
-      
+
       // L'avant fond
       p.drawForeground(g, vs);
-      
+
       // Quelques overlays
 //      vs.drawScale(g, vs, 0, 0);
 //      vs.drawGrid(g, null, 0, 0);
 //      vs.drawLabel(g,0, 0);
 //      vs.drawSize(g,0, 0);
 //      vs.drawNE(g,p.projd,0, 0);
-      
+
       // Je génère le flux de l'image finale
       ImageIO.write(img, fmt, output);
    }
-   
-   static protected Aladin createAladin() throws Exception {
+
+   static public Aladin createAladin() throws Exception {
       Aladin aladin = new Aladin();
       aladin.creatFonts();
       aladin.levelTrace=3;
@@ -89,24 +100,24 @@ public class ImageMaker {
    }
 
    static public void main(String [] argv) {
-      int n=1;
-      long t0=0;
-      try {
+       int n=1;
+       long t0=0;
+       try {
 
-         Aladin aladin = createAladin();
-         t0 = System.currentTimeMillis();
+          Aladin aladin = createAladin();
+          t0 = System.currentTimeMillis();
 
-         for( int i=0; i<n; i++ ) {
-            FileOutputStream output = new FileOutputStream( new File("/Test"+i+".jpg"));
-            new ImageMaker(aladin,output, "M101 test", 15000,15000, "jpeg", new Coord(210.80242,+54.34875/*056.61431,+24.13817*/), 2);
-         }
+          for( int i=0; i<n; i++ ) {
+             FileOutputStream output = new FileOutputStream( new File("/Test"+i+".jpg"));
+             new ImageMaker(aladin,output, "M101 test", 15000,15000, "jpeg", new Coord(210.80242,+54.34875/*056.61431,+24.13817*/), 2);
+          }
 
-         long t1 =System.currentTimeMillis();
-         System.out.println("Image generated in "+(t1-t0)/n+"ms");
-         
-      }catch( Exception e ) {
-         e.printStackTrace();
-      }
-      System.exit(0);
-   }
+          long t1 =System.currentTimeMillis();
+          System.out.println("Image generated in "+(t1-t0)/n+"ms");
+
+       }catch( Exception e ) {
+          e.printStackTrace();
+       }
+       System.exit(0);
+    }
 }
